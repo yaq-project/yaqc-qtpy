b40fcf16ab01b859a4af81ed380a9b3e92ef54a8
Blaise Thompson <blaise@untzag.com>
2021-11-18 13:58:11 -0600

    internal refactor

--- yaqc_qtpy/_main_widget.py
+++ yaqc_qtpy/_main_widget.py
@@ -4,8 +4,8 @@ import yaqc
 import numpy as np
 import entrypoints
 
-from ._properties_table_widget import PropertiesTableWidget
-from ._main_by_traits import MainByTraits
+from ._has_position_widget import HasPositionWidget
+from ._is_sensor_widget import IsSensorWidget
 from ._plot import Plot1D
 
 
@@ -15,7 +15,10 @@ class MainWidget(QtWidgets.QTabWidget):
         super().__init__(parent=parent)
         self.qclient = qclient
         self.addTab(QtWidgets.QLabel("TODO"), "config")
-        self.addTab(MainByTraits(qclient=self.qclient, parent=self), "traits")
+        if "has-position" in self.qclient.traits:
+            self.addTab(HasPositionWidget(qclient=self.qclient, parent=self), "traits")
+        if "is-sensor" in self.qclient.traits:
+            self.addTab(IsSensorWidget(qclient=self.qclient, parent=self), "traits")
         # gui tabs provided via entrypoints
         group = f"yaqc_qtpy.main.{self.qclient._client._protocol['protocol']}"
         for ep in entrypoints.get_group_all(group):
