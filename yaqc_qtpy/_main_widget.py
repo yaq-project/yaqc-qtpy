c0042c2f06388f91077743cf9c7cc470f5e0cb09
Kyle Sunden <git@ksunden.space>
2021-12-09 20:38:12 +0000

    Add a console tab with a jupyter qtconsole widget to the main window section

--- yaqc_qtpy/_main_widget.py
+++ yaqc_qtpy/_main_widget.py
@@ -4,17 +4,46 @@ import yaqc
 import numpy as np
 import entrypoints
 
+from qtconsole.rich_jupyter_widget import RichJupyterWidget
+from qtconsole.manager import QtKernelManager
+
 from ._config_widget import ConfigWidget
 from ._has_position_widget import HasPositionWidget
 from ._is_sensor_widget import IsSensorWidget
 from ._plot import Plot1D
 
+# The ID of an installed kernel, e.g. 'bash' or 'ir'.
+USE_KERNEL = 'python3'
+
+# This function was copied from the qtconsole embedding example code
+# https://github.com/jupyter/qtconsole/blob/b4e08f763ef1334d3560d8dac1d7f9095859545a/examples/embed_qtconsole.py#L19
+def make_jupyter_widget_with_kernel():
+    """Start a kernel, connect to it, and create a RichJupyterWidget to use it
+    """
+    kernel_manager = QtKernelManager(kernel_name=USE_KERNEL)
+    kernel_manager.start_kernel()
+
+    kernel_client = kernel_manager.client()
+    kernel_client.start_channels()
+
+    jupyter_widget = RichJupyterWidget()
+    jupyter_widget.set_default_style("linux")  # Dark bg color.... only key to get it...
+    jupyter_widget.kernel_manager = kernel_manager
+    jupyter_widget.kernel_client = kernel_client
+    return jupyter_widget
 
 class MainWidget(QtWidgets.QTabWidget):
     def __init__(self, qclient, *, parent=None):
         super().__init__(parent=parent)
         self.qclient = qclient
         self.addTab(ConfigWidget(qclient=self.qclient, parent=self), "config")
+        ipy = make_jupyter_widget_with_kernel()
+        ipy.execute(f"""import yaqc
+c = yaqc.Client(host='{self.qclient.host}', port={self.qclient.port})
+{self.qclient.id()["name"]} = c
+""")
+        print(dir(ipy))
+        self.addTab(ipy, "console")
         if "has-position" in self.qclient.traits:
             self.addTab(HasPositionWidget(qclient=self.qclient, parent=self), "has-position")
         if "is-sensor" in self.qclient.traits:
